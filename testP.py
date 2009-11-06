0a40deb61a68117d15b30c51029c32c972e3abb7
Robert BuRnEr Schadek <realburner@gmx.de>
2009-11-06 17:09:24 +0100

    merge

--- testP.py
+++ testP.py
@@ -1,7 +1,7 @@
 import Graph
 from System import System as System
 
-foo = Graph.Graph(60,4,8)
+foo = Graph.Graph(60,5,10)
 #uncommit this to print graph to dotGr.jpg
 #foo.printGraph()
 
@@ -9,12 +9,19 @@ foo = Graph.Graph(60,4,8)
 #print "\n\n\n"
 
 # numPeers, minNumWr, maxNumWr, numTests
-bar = System(foo, 50, 2, 15, 400)
+bar = System(foo,150, 2, 15, 400)
 bar.sim()
-bar.makeResult()
+#bar.makeResult()
 
 rightCnt = 0
-for x in bar.right:
-	if x[0]:
-		rightCnt+=1
-print "\n\nRight " + str(rightCnt) + " of " + str(bar.numTests)
+sum = 0
+foo = Graph.Graph(60,4,8)
+for i in range(10):
+	bar = System(foo, 50, 2, 15, 400)
+	bar.sim()
+	sum+=bar.numTests
+	for x in bar.right:
+		if x[0]:
+			rightCnt+=1
+
+print "\n\nRight " + str(rightCnt) + " of " + str(sum)
